0d83c4332d73e25e3cfd1549e0cb30e6a550f988
Francisco Rian <73894567+FRianOD@users.noreply.github.com>
2023-11-21 20:05:44 -0300

    first commit

---
+++ Fila.py
@@ -0,0 +1,54 @@
+class Dado:
+    #Construtor do Dado
+    def __init__(self, valor):
+        #Atributos setados com os valores padrões
+        self.valor = valor
+        self.prox = None
+        
+class Fila:
+    #Construtor da Fila
+    def __init__(self):
+        #Atributos setados com os valores padrões
+        self.inicio = None
+        self.fim = None
+        self.numeroDeElementos = 0
+        
+    def enfileirar(self, valor):
+        novoDado = Dado(valor)
+        if self.estaVazia() is True:
+            self.inicio = self.fim = novoDado
+        else: 
+            self.fim.prox = novoDado
+            self.fim = novoDado
+        self.numeroDeElementos += 1
+    
+    def desenfileirar(self):
+        if self.estaVazia() is True:
+            print("Fila vazia")
+            return None
+        lixo = self.inicio
+        self.inicio = self.inicio.prox
+        self.numeroDeElementos -= 1
+        return lixo.valor
+    
+    def tamanho(self):
+        return self.numeroDeElementos
+    
+    def estaVazia(self):
+        if self.fim is None: return True
+        else: return False
+    
+    def Busca(self, valor):
+        pass
+    
+fila = Fila()
+fila.enfileirar(10)
+fila.enfileirar(20)
+fila.enfileirar(30)
+
+print(f"Tamanho da fila : {fila.tamanho()}")
+
+lixo = fila.desenfileirar()
+print(f"Elemento retirado : {lixo}")
+
+print(f"Tamanho da lista apos a remoção : {fila.tamanho()}")

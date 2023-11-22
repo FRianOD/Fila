fa9e1ac5660aa1fec07d52308b2f95ea496d6e9d
Francisco Rian <73894567+FRianOD@users.noreply.github.com>
2023-11-22 18:47:58 -0300

    Fila v0.2

--- Fila.py
+++ Fila.py
@@ -14,41 +14,67 @@ class Fila:
         self.numeroDeElementos = 0
         
     def enfileirar(self, valor):
+        
         novoDado = Dado(valor)
+        
         if self.estaVazia() is True:
             self.inicio = self.fim = novoDado
+        
         else: 
             self.fim.prox = novoDado
             self.fim = novoDado
+        
         self.numeroDeElementos += 1
     
     def desenfileirar(self):
+        
         if self.estaVazia() is True:
             print("Fila vazia")
             return None
+        
         lixo = self.inicio
         self.inicio = self.inicio.prox
         self.numeroDeElementos -= 1
         return lixo.valor
     
     def tamanho(self):
+        
         return self.numeroDeElementos
     
     def estaVazia(self):
+        
         if self.fim is None: return True
+        
         else: return False
     
-    def Busca(self, valor):
-        pass
-    
-fila = Fila()
-fila.enfileirar(10)
-fila.enfileirar(20)
-fila.enfileirar(30)
-
-print(f"Tamanho da fila : {fila.tamanho()}")
-
-lixo = fila.desenfileirar()
-print(f"Elemento retirado : {lixo}")
-
-print(f"Tamanho da lista apos a remoção : {fila.tamanho()}")
+    def buscar(self, valor):
+        
+        if self.estaVazia() is None: return "Fila vazia"
+        
+        temp = self.inicio
+        
+        for i in range(self.numeroDeElementos):
+            
+            if temp.valor == valor: return f"O valor {temp.valor} esta na fila !!"
+            
+            else:
+                i+= 1
+                temp = temp.prox
+                
+        else: return f"O valor {valor} não esta na fila !!"
+        
+    def mostrarFila(self):
+        
+        if self.estaVazia() is None: return "Fila vazia"
+        
+        temp = self.inicio
+        
+        print("[", end="")
+        for i in range(self.numeroDeElementos-1):
+            
+            print(f"{temp.valor}", end=", ")
+            temp = temp.prox
+            i+=1
+        
+        else:
+            print(f"{temp.valor}]")
